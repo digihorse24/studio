30df89e41fbdb78bdf9f5876f49f82c3c2e8cb2c
Pascal Schmid <teamhufmanager@gmail.com>
2025-10-27 06:33:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-561398427-70646",
+  "appId": "1:506923802811:web:d437772fa5b9b2064c899e",
+  "apiKey": "AIzaSyDBOi_r9dcxRpicP9_d6-j1jK8qjXKG-kE",
+  "authDomain": "studio-561398427-70646.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "506923802811"
+};
